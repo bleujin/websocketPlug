ea89bb59036aa6d482b45a72936d0115b8e27c29
bleujin <bleujin@gmail.com>
2012-09-18 10:36:31 +0900

    ..

--- src/net/ion/nchat/util/FileFinder.java
+++ src/net/ion/nchat/util/FileFinder.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 
 import net.ion.radon.core.IService;
+import net.ion.radon.core.config.PlugInsConfiguration;
 import net.ion.radon.core.context.OnOrderEventObject;
 
 public class FileFinder implements OnOrderEventObject {
@@ -16,7 +17,13 @@ public class FileFinder implements OnOrderEventObject {
 
 	public File findFile(IService service, String relativePath) {
 		try {
-			File foundFile = service.getAradon().getGlobalConfig().plugin().findPlugInFile(pluginId, "/" + relativePath);
+			PlugInsConfiguration pluginConfig = service.getAradon().getGlobalConfig().plugin();
+			File foundFile = null ;
+			try {
+				foundFile = pluginConfig.findPlugInFile(pluginId, "/" + relativePath);
+			} catch(IllegalArgumentException notfound){
+				foundFile = new File(relativePath);
+			}
 			if (!foundFile.exists())
 				throw new IllegalStateException(foundFile.getCanonicalPath() + " not exists");
 			return foundFile;
