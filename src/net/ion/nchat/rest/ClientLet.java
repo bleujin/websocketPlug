736f56ffde3673bc999346b08450682531e015a6
bleujin <bleujin@gmail.com>
2012-09-18 10:58:30 +0900

    ..

--- src/net/ion/nchat/rest/ClientLet.java
+++ src/net/ion/nchat/rest/ClientLet.java
@@ -4,16 +4,21 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.InetAddress;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.net.UnknownHostException;
 import java.util.Map;
 
 import net.ion.framework.parse.gson.JsonObject;
+import net.ion.framework.util.Debug;
 import net.ion.framework.util.IOUtil;
 import net.ion.framework.util.MapUtil;
 import net.ion.framework.util.ObjectUtil;
+import net.ion.framework.util.StringUtil;
 import net.ion.nchat.util.FileFinder;
 import net.ion.radon.core.config.AradonConstant;
 import net.ion.radon.core.let.AbstractServerResource;
@@ -56,12 +61,14 @@ public class ClientLet extends AbstractServerResource {
 		return new StringRepresentation(st.toString(), MediaType.TEXT_HTML, Language.ALL);
 	}
 
-	protected JsonObject getTargetWebsocketServer(String topicId, String userId, String userIp) throws URISyntaxException, UnknownHostException {
+	protected JsonObject getTargetWebsocketServer(String topicId, String userId, String userIp) throws URISyntaxException, MalformedURLException, IOException {
 		String authKey = "0";
 		Object port = getContext().getAttributeObject(AradonConstant.CONFIG_PORT) ;
 		String portString = ObjectUtil.toString(port, "9000") ;
 
-		URI path = new URI("ws://" + InetAddress.getLocalHost().getHostAddress() + ":" + portString + "/async/chat/" + topicId + "/" + userId);
+		
+		
+		URI path = new URI("ws://" + findPublicIp() + ":" + portString + "/async/chat/" + topicId + "/" + userId);
 
 		JsonObject jsonObject = new JsonObject();
 		jsonObject.put("path", path.toString());
@@ -69,5 +76,12 @@ public class ClientLet extends AbstractServerResource {
 		jsonObject.put("authkey", authKey);
 
 		return jsonObject;
+	}
+	
+	private String findPublicIp() throws MalformedURLException, IOException {
+		InputStream in = new URL("http://ip-echo.appspot.com/").openStream() ;
+		String publicIP = StringUtil.trim(IOUtil.toString(in)) ;
+		in.close() ;
+		return publicIP ;
 	}
 }
